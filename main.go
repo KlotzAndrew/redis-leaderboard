c61ab45270174b00186a992de44de333cbccddf5
Andrew Klotz <agc.klotz@gmail.com>
2017-12-23 22:20:08 -0500

    store/fetch data in redis

--- main.go
+++ main.go
@@ -5,29 +5,57 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+  "github.com/go-redis/redis"
 )
 
 func viewRank(w http.ResponseWriter, r *http.Request) {
   vars:= mux.Vars(r)
   id := vars["id"]
-	fmt.Fprintf(w, "got rank %s", id)
+
+  client := redisClient()
+  zRank := client.ZRank("leaderboard", id)
+
+	fmt.Fprintf(w, "got rank %d", zRank.Val())
 }
 
 func viewLeaderboard(w http.ResponseWriter, r *http.Request) {
-  vars:= mux.Vars(r)
+  vars := mux.Vars(r)
   id := vars["id"]
-	fmt.Fprintf(w, "got leaderboard %s", id)
+
+  client := redisClient()
+  zRank := client.ZRank("leaderboard", id)
+
+  lower := zRank.Val() - 5
+  upper := zRank.Val() + 4
+
+  zRangeWithScores := client.ZRangeWithScores("leaderboard", lower, upper)
+
+	fmt.Fprintf(w, "got leaderboard %v", zRangeWithScores.Val())
 }
 
 func updateRank(w http.ResponseWriter, r *http.Request) {
   vars:= mux.Vars(r)
   id := vars["id"]
-	fmt.Fprintf(w, "updating rank %s", id)
+
+  client := redisClient()
+  zAdd := client.ZAdd("leaderboard", redis.Z{100, id})
+
+	fmt.Fprintf(w, "updating rank %d", zAdd.Val())
+}
+
+func redisClient() *redis.Client {
+  client := redis.NewClient(&redis.Options{
+    Addr:     "localhost:6379",
+    Password: "", // no password set
+    DB:       0,  // use default DB
+  })
+
+  return client
 }
 
 func main() {
 	r := mux.NewRouter()
-  r.HandleFunc("/leaderboard", viewLeaderboard).Methods("GET")
+  r.HandleFunc("/leaderboard/{id}", viewLeaderboard).Methods("GET")
 	r.HandleFunc("/rank/{id}", viewRank).Methods("GET")
 	r.HandleFunc("/rank/{id}", updateRank).Methods("POST")
 
