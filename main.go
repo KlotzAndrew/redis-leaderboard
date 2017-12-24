ab5937735e820221228716fa10ebaad6aa80c7d3
Andrew Klotz <agc.klotz@gmail.com>
2017-12-24 12:25:16 -0500

    respond with json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -8,65 +9,112 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func viewRank(w http.ResponseWriter, r *http.Request) {
+func (a *app) viewRank(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	client := redisClient()
-	zRank := client.ZRank("leaderboard", id)
+	zRank := a.Redis.ZRank("leaderboard", id)
+	user := user{ID: id, Rank: zRank.Val()}
 
-	fmt.Fprintf(w, "got rank %d", zRank.Val())
+	respondWithJSON(w, http.StatusOK, user)
 }
 
-func viewLeaderboard(w http.ResponseWriter, r *http.Request) {
+func (a *app) viewLeaderboard(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	client := redisClient()
-	zRank := client.ZRank("leaderboard", id)
+	zRank := a.Redis.ZRank("leaderboard", id)
 
 	lower := zRank.Val() - 5
 	upper := zRank.Val() + 4
 
-	zRangeWithScores := client.ZRangeWithScores("leaderboard", lower, upper)
+	zRangeWithScores := a.Redis.ZRangeWithScores("leaderboard", lower, upper)
 
-	fmt.Fprintf(w, "got leaderboard %v", zRangeWithScores.Val())
+	users := []user{}
+	for _, data := range zRangeWithScores.Val() {
+		member, _ := data.Member.(string)
+
+		user := user{ID: data.Member, Score: data.Score}
+		users = append(users, user)
+	}
+
+	respondWithJSON(w, http.StatusOK, users)
 }
 
-func updateRank(w http.ResponseWriter, r *http.Request) {
+func (a *app) updateRank(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	client := redisClient()
-	zAdd := client.ZAdd("leaderboard", redis.Z{100, id})
+	zAdd := a.Redis.ZAdd("leaderboard", redis.Z{100, id})
+	user := user{ID: id, Rank: zAdd.Val()}
 
-	fmt.Fprintf(w, "updating rank %d", zAdd.Val())
+	respondWithJSON(w, http.StatusOK, user)
 }
 
-func topRanks(w http.ResponseWriter, r *http.Request) {
-	client := redisClient()
+func (a *app) topRanks(w http.ResponseWriter, r *http.Request) {
+	zRevRangeWithScores := a.Redis.ZRevRangeWithScores("leaderboard", 0, 9)
 
-	zRevRangeWithScores := client.ZRevRangeWithScores("leaderboard", 0, 9)
+	users := []user{}
+	for _, data := range zRevRangeWithScores.Val() {
+		member, _ := data.Member.(string)
 
-	fmt.Fprintf(w, "topScores %v", zRevRangeWithScores.Val())
+		user := user{ID: member, Score: data.Score}
+		users = append(users, user)
+	}
+
+	respondWithJSON(w, http.StatusOK, users)
 }
 
-func redisClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{
+func respondWithJSON(w http.ResponseWriter, code int, user interface{}) {
+	response, _ := json.Marshal(user)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+type user struct {
+	ID    string  `json:"id"`
+	Rank  int64   `json:"rank"`
+	Score float64 `json:"score"`
+}
+
+type app struct {
+	Router *mux.Router
+	Redis  *redis.Client
+}
+
+func (a *app) initialize() {
+	a.Router = mux.NewRouter()
+	a.Redis = redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	return client
+	a.initializeRoutes()
+}
+
+// "/product/{id:[0-9]+}"
+// vars := mux.Vars(r)
+// id, err := strconv.Atoi(vars["id"])
+// if err != nil {
+// 		respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
+// 		return
+// }
+func (a *app) initializeRoutes() {
+	a.Router.HandleFunc("/leaderboard/{id}", a.viewLeaderboard).Methods("GET")
+	a.Router.HandleFunc("/rank/{id}", a.viewRank).Methods("GET")
+	a.Router.HandleFunc("/rank/{id}", a.updateRank).Methods("POST")
+	a.Router.HandleFunc("/topranks", a.topRanks).Methods("GET")
+}
+
+func (a *app) run() {
+	http.ListenAndServe(":8080", a.Router)
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/leaderboard/{id}", viewLeaderboard).Methods("GET")
-	r.HandleFunc("/rank/{id}", viewRank).Methods("GET")
-	r.HandleFunc("/rank/{id}", updateRank).Methods("POST")
-	r.HandleFunc("/topranks", topRanks).Methods("GET")
-
-	http.ListenAndServe(":8080", r)
+	a := app{}
+	a.initialize()
+	a.run()
 }
