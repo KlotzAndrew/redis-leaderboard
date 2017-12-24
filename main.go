7f3ec381d30dea65d25a789a066686d4d054f1fa
Andrew Klotz <agc.klotz@gmail.com>
2017-12-23 22:25:08 -0500

    go fmt

--- main.go
+++ main.go
@@ -4,58 +4,58 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
-  "github.com/go-redis/redis"
 )
 
 func viewRank(w http.ResponseWriter, r *http.Request) {
-  vars:= mux.Vars(r)
-  id := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-  client := redisClient()
-  zRank := client.ZRank("leaderboard", id)
+	client := redisClient()
+	zRank := client.ZRank("leaderboard", id)
 
 	fmt.Fprintf(w, "got rank %d", zRank.Val())
 }
 
 func viewLeaderboard(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  id := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-  client := redisClient()
-  zRank := client.ZRank("leaderboard", id)
+	client := redisClient()
+	zRank := client.ZRank("leaderboard", id)
 
-  lower := zRank.Val() - 5
-  upper := zRank.Val() + 4
+	lower := zRank.Val() - 5
+	upper := zRank.Val() + 4
 
-  zRangeWithScores := client.ZRangeWithScores("leaderboard", lower, upper)
+	zRangeWithScores := client.ZRangeWithScores("leaderboard", lower, upper)
 
 	fmt.Fprintf(w, "got leaderboard %v", zRangeWithScores.Val())
 }
 
 func updateRank(w http.ResponseWriter, r *http.Request) {
-  vars:= mux.Vars(r)
-  id := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-  client := redisClient()
-  zAdd := client.ZAdd("leaderboard", redis.Z{100, id})
+	client := redisClient()
+	zAdd := client.ZAdd("leaderboard", redis.Z{100, id})
 
 	fmt.Fprintf(w, "updating rank %d", zAdd.Val())
 }
 
 func redisClient() *redis.Client {
-  client := redis.NewClient(&redis.Options{
-    Addr:     "localhost:6379",
-    Password: "", // no password set
-    DB:       0,  // use default DB
-  })
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
-  return client
+	return client
 }
 
 func main() {
 	r := mux.NewRouter()
-  r.HandleFunc("/leaderboard/{id}", viewLeaderboard).Methods("GET")
+	r.HandleFunc("/leaderboard/{id}", viewLeaderboard).Methods("GET")
 	r.HandleFunc("/rank/{id}", viewRank).Methods("GET")
 	r.HandleFunc("/rank/{id}", updateRank).Methods("POST")
 
